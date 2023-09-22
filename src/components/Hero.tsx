70c32162e4d9e14362240816a596208ad1055c5d
Om Patel <omunite21@gmail.com>
2023-09-22 14:19:25 +0530

    Fixed navbar overlay in mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,22 +8,17 @@ const Hero = () => {
       <div className="flex-1 flex justify-center items-start flex-col xl:px-0 sm:px-16 px-6">
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="h-[32px] w-[32px]" loading="lazy"/>
-          <Fade cascade delay={1e2} damping={1e-2}>
+       
             <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] ml-2">
               <span className="text-white">20%</span> Discount For{" "}
               <span className="text-white">1 Month</span> Account
             </p>
-          </Fade>
         </div>
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            <Fade cascade delay={1e3} damping={1e-1}>
               The Next
-            </Fade>{" "}
             <br className="sm:block hidden" />{" "}
-            <Fade cascade delay={1e3} damping={1e-1}>
               <span className="text-gradient">Generation</span>
-            </Fade>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <Fade>
